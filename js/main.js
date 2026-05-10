f33dafc000db8eaf8815da728c1e63e24d456afb
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-10 17:56:02 +0200

    añadidas funciones iniciales para conectar con el API de Pexels
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const API_KEY = 'TewSBIvF8QDVsqGh0UOmnufPlk6r9JM6l2OIJs2MOoFtuvtZbQPy2t3E';
+const IDIOMA = 'es-ES';
+const IMAGENES_POR_PAGINA = 9;
+
+const consultaUrl = async (url) => {
+  try {
+    const consulta = fetch(url, {
+      headers: {
+        'Authorization': API_KEY,
+      },
+    });
+    return consulta;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const consultaFiltro = async (filtro) => {
+  try {
+    const respuesta = await consultaUrl(`https://api.pexels.com/v1/search?locale=${IDIOMA}&per_page=${IMAGENES_POR_PAGINA}&query=${filtro}`);
+    if (!respuesta.ok) throw respuesta.status;
+    const datos = await respuesta.json();
+    if (typeof datos.total_results === 'undefined') throw `Error: Recibiendo las imágenes del filtro ${filtro}`;
+    if (datos.total_results === 0) throw `Error: No existen imágenes para el filtro ${filtro}`;
+    return datos;
+  } catch (error) {
+    console.log('consultaFiltro:', error);
+  }
+};
+
+const pintarDatos = async () => {
+  try {
+    const datos = await consultaFiltro('arboles');
+    console.log(datos);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+pintarDatos();
