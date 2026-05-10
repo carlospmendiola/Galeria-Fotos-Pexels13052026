30da0a876e90beba17be2e143960b6ed6b5e9a54
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-10 23:11:17 +0200

    añadida documentación de los tipos de datos de Pexels y ajustados parámetros de búsqueda como los del API de Pexels
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,25 +1,86 @@
+/**
+ * Un recurso Photo de Pexels.
+ * @typedef {Object} PexelsPhoto
+ * @property {number} id El id de la foto.
+ * @property {number} width El ancho real de la foto en píxeles.
+ * @property {number} height El alto real de la foto en píxeles.
+ * @property {string} url La URL de Pexels donde se encuentra la foto.
+ * @property {string} photographer El nombre del fotógrafo que tomó la foto.
+ * @property {string} photographer_url La URL del perfil de Pexels del fotógrafo.
+ * @property {number} photographer_id El id del fotógrafo.
+ * @property {string} avg_color El color promedio de la foto. Útil para un marcador de posición mientras la foto carga.
+ * @property {PexelsPhotoSize} src Un surtido de los diferentes tamaños de imagen que pueden ser usados para mostrar esta foto.
+ * @property {boolean} liked Si la imagen está marcada como me gusta para el usuario de Pexels cuya API accede.
+ * @property {string} alt Descripción textual de la foto para usar en el atributo alt.
+ */
+
+/**
+ * Tamaños de Photo de Pexels.
+ * @typedef {Object} PexelsPhotoSize
+ * @property {string} original La imagen sin ningún cambio de tamaño. Será la misma que las propiedades width y height.
+ * @property {string} large2x La imagen redimensionada a W 940px X H 650px DPR 1.
+ * @property {string} large La imagen redimensionada a W 940px X H 650px DPR 2.
+ * @property {string} medium La imagen escalada proporcionalmente a una altura de 350px.
+ * @property {string} small La imagen escalada proporcionalmente a una altura de 130px.
+ * @property {string} portrait La imagen recortada a W 800px X H 1200px.
+ * @property {string} landscape La imagen recortada a W 1200px X H 627px.
+ * @property {string} tiny La imagen recortada a W 280px X H 200px.
+ */
+
+/*
+  TODO: No se si se pueden definir propiedades opcionales en los objetos, por ejemplo prev_page y next_page son string o opcional
+  y no se si ponerlas como están ahora entre corchetes o bien como {string|undefined}.
+*/
+/**
+ * Una respuesta de búsqueda en la API de Pexels.
+ * @typedef {Object} PexelsSearchResponse
+ * @property {PexelsPhoto[]} photos Un array de objetos PexelsPhoto.
+ * @property {number} page El número de página actual.
+ * @property {number} per_page El número de resultados devueltos con cada página.
+ * @property {number} total_results El número total de resultados para la petición.
+ * @property {string} [prev_page] URL para la página anterior de los resultados, si aplica.
+ * @property {string} [next_page] URL para la página siguiente de los resultados, si aplica.
+ */
+
 const API_KEY = 'TewSBIvF8QDVsqGh0UOmnufPlk6r9JM6l2OIJs2MOoFtuvtZbQPy2t3E';
-const IDIOMA = 'es-ES';
-const IMAGENES_POR_PAGINA = 9;
 const fragmento = document.createDocumentFragment();
 const pintarGaleria = document.querySelector('#pintarGaleria');
 
-const consultaUrl = async (url) => {
+// Variables globales y valores por defecto de parámetros de búsqueda de Pexels.
+// TODO: Definir ¿enumeraciones? para los valores de las opciones.
+let orientacion = 'landscape';
+let tamanio = 'small';
+let color = undefined;
+let idiomaConsulta = 'es-ES';
+let imagenesPorPagina = 9;
+
+/**
+ * Petición a Pexels
+ * @param {*} accion Acción a solicitar al API de Pexels.
+ * @returns {Promise<Response | undefined>}
+ */
+const peticionPexels = async (accion) => {
   try {
-    const consulta = fetch(url, {
+    const peticion = fetch(`https://api.pexels.com/v1/${accion}`, {
       headers: {
         'Authorization': API_KEY,
       },
     });
-    return consulta;
+    return peticion;
   } catch (error) {
     console.log(error);
   }
 };
 
-const consultaFiltro = async (filtro, pagina = 1) => {
+/**
+ *
+ * @param {string} consulta Filtro por el que buscar.
+ * @param {number} pagina Página del resultado de búsqueda a recoger.
+ * @returns {Promise<PexelsSearchResponse>}
+ */
+const buscarFotos = async (consulta, pagina = 1) => {
   try {
-    const respuesta = await consultaUrl(`https://api.pexels.com/v1/search?locale=${IDIOMA}&page=${pagina}&per_page=${IMAGENES_POR_PAGINA}&query=${filtro}`);
+    const respuesta = await peticionPexels(`search?locale=${idiomaConsulta}&page=${pagina}&per_page=${imagenesPorPagina}&query=${consulta}`);
     if (!respuesta.ok) throw respuesta.status;
     const datos = await respuesta.json();
     if (typeof datos.total_results === 'undefined') throw `Error: Recibiendo las imágenes del filtro ${filtro}`;
@@ -32,7 +93,7 @@ const consultaFiltro = async (filtro, pagina = 1) => {
 
 const pintarDatos = async (filtro, pagina) => {
   try {
-    const datos = await consultaFiltro(filtro, pagina);
+    const datos = await buscarFotos(filtro, pagina);
     console.log(datos);
 
     datos.photos.forEach(foto => {
